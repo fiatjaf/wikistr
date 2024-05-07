33b95ac2acb0f97fb6ae0d0010805cf6a9079b9b
fiatjaf <fiatjaf@gmail.com>
2024-05-07 00:34:13 -0300

    no close buttons on welcome and new tabs, move close button to the right.

--- src/components/CardElement.svelte
+++ src/components/CardElement.svelte
@@ -120,17 +120,21 @@
       ? hashbow(tab.data, 88)
       : ''}
 >
-  <button on:click={close}
-    ><svg
-      xmlns="http://www.w3.org/2000/svg"
-      fill="none"
-      viewBox="0 0 24 24"
-      stroke-width="1.5"
-      stroke="currentColor"
-      class="w-6 h-6"
-      ><path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" /></svg
-    ></button
-  >
+  {#if tab.type !== 'welcome' && tab.type !== 'new'}
+    <div class="flex justify-end">
+      <button on:click={close}>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke-width="1.5"
+          stroke="currentColor"
+          class="w-6 h-6"
+          ><path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" /></svg
+        >
+      </button>
+    </div>
+  {/if}
   <article class="font-sans mx-auto p-2 lg:max-w-4xl">
     {#if tab.type === 'article'}
       <Article {createChild} {replaceSelf} article={tab.data} {tab} />
